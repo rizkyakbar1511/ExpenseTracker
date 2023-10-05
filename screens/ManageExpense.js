e3b98b147f7a7eafee4618669a7d7a7256414a68
Rizki Akbar <rizkyakbar1511@gmail.com>
2023-10-06 00:40:22 +0700

    first commit

---
+++ screens/ManageExpense.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function ManageExpense() {
+  return (
+    <View>
+      <Text>ManageExpense</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function RecentExpenses() {
+  return (
+    <View>
+      <Text>RecentExpenses</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
